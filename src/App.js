9a1dd86e9173b22d608260a0d4bade6c0fe8de5e
Michael Oldacre <mloldacre@live.com>
2020-07-29 21:20:32 -0400

    Removed Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Test from './Test';
 import CustomizeView from './CustomizeView';
 import CartView from './CartView';
 
